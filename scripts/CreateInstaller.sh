1f6ce101a780d9148fc291d0e6063589a6784c67
perelandra <perelandra@86950100-d00d-4dca-8e76-ff5d7b6a88f3>
2012-01-21 14:05:30 +0000

    Added EngineSubscriber classes to the EngineSubscriberKit.

--- scripts/CreateInstaller.sh
+++ scripts/CreateInstaller.sh
@@ -21,7 +21,7 @@ cp -u $scriptsDir/InstallPackage/Install.sh $releasesDir/$1
 cp -u $docsDir/InstallerReadMe.pdf $releasesDir/$1/ReadMe
 zip -j -o -u $releasesDir/$1/Einsteinium.pkg $docsDir/ReadMe $docsDir/License $srcDir/Daemon/einsteinium_daemon $srcDir/Engine/einsteinium_engine $srcDir/Launcher/Einsteinium_Launcher $srcDir/Preferences/Einsteinium_Preferences $scriptsDir/InstallPackage/Uninstall.sh
 mkdir $releasesDir/$1/EngineSubscriberKit
-cp $srcDir/Engine/SubscriberExample/makefile $srcDir/Engine/SubscriberExample/subscriber_example $srcDir/Engine/SubscriberExample/subscriber_example.rsrc $srcDir/Engine/SubscriberExample/SubscriberExample.cpp "$docsDir/Engine Subscriber's Development Guide" $releasesDir/$1/EngineSubscriberKit
+cp $srcDir/Engine/SubscriberExample/makefile $srcDir/Engine/SubscriberExample/subscriber_example $srcDir/Engine/SubscriberExample/subscriber_example.rsrc $srcDir/Engine/SubscriberExample/SubscriberExample.cpp "$docsDir/Engine Subscriber's Development Guide" $srcDir/Engine/EngineSubscriber.* $releasesDir/$1/EngineSubscriberKit
 cd $releasesDir/$1
 zip -o -r -u Einsteinium.pkg EngineSubscriberKit
 rm -r EngineSubscriberKit
