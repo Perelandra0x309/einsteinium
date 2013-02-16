7bcd3e1c7177d1323af9732a5000c70ee54db443
perelandra <perelandra@86950100-d00d-4dca-8e76-ff5d7b6a88f3>
2013-02-16 05:06:51 +0000

    Set the Engine thread priority to low.

--- src/Engine/main.cpp
+++ src/Engine/main.cpp
@@ -6,7 +6,13 @@
 
 
 int main()
-{	einsteinium_engine myApp;//create application
+{
+	einsteinium_engine myApp;//create application
+	thread_info info;
+	int32 cookie=0;
+	while(get_next_thread_info(0, &cookie, &info) == B_OK){
+		set_thread_priority(info.thread, B_LOW_PRIORITY);
+	}
 	myApp.Run();//run application
 	return 0;
 }
