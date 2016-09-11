70a3c3956b5b36f0d0eae524780bd764e3a6809d
perelandra <perelandra@86950100-d00d-4dca-8e76-ff5d7b6a88f3>
2016-09-11 19:52:51 +0000

    Update post install script.

--- scripts/einsteinium_hpkg_postinstall.sh
+++ scripts/einsteinium_hpkg_postinstall.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
-appDir="/boot/system/apps/Einsteinium"
-binDir="/boot/system/bin"
 
 # Start Einsteinium now
-$binDir/einsteinium_engine &
+open application/x-vnd.Einsteinium_Engine &
 sleep 1
-$binDir/einsteinium_daemon &
-open $appDir
-open $appDir/ReadMe
+open application/x-vnd.Einsteinium_Daemon &
+desklink "cmd=Remove replicant:desklink --remove=Einsteinium_Launcher" application/x-vnd.Einsteinium_Launcher
 
 # Notification to user
-alert --modal "Einsteinium has been successfully installed.  You may uninstall Einsteinium by removing the einsteinium hpkg file from /boot/system/packages." "OK"
+notify --group Einsteinium --onClickApp application/x-vnd.Einsteinium_Launcher --timeout 60 --icon \
+"/boot/home/config/settings/Einsteinium/notify_icon" \
+"Thank you for installing Einsteinium.  Click on this message to open the Launcher application."
+
+# Open ReadMe file
+if test -f /boot/system/apps/Einsteinium/ReadMe
+then
+	open /boot/system/apps/Einsteinium/ReadMe
+#	notify --group Einsteinium --onClickFile /boot/system/apps/Einsteinium/ReadMe --timeout 60 --icon \
+#"/boot/home/config/settings/Einsteinium/notify_icon" \
+#"Click on this message to open the ReadMe file."
+elif test -f /boot/home/config/apps/Einsteinium/ReadMe
+then
+	open /boot/home/config/apps/Einsteinium/ReadMe
+#	notify --group Einsteinium --onClickFile "/boot/home/config/apps/Einsteinium/ReadMe" --timeout 60 --icon \
+#"/boot/home/config/settings/Einsteinium/notify_icon" \
+#"Click on this message to open the ReadMe file."
+fi
 
 exit 0
