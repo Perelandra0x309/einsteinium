073ada1f0bcc70d9c5f2b8fffbbe141c84c685af
Brian Hill <supernova@warpmail.net>
2016-10-08 18:56:29 +0000

    Created EinsteiniumBootscript.sh to start daemon and engine on startup.

--- scripts/einsteinium_hpkg_postinstall.sh
+++ scripts/einsteinium_hpkg_postinstall.sh
@@ -8,7 +8,6 @@ open application/x-vnd.Einsteinium_Daemon &
 #find app and data paths
 appDir=""
 dataDir=""
-binDir=""
 sysAppsDir=`finddir B_SYSTEM_APPS_DIRECTORY`
 userAppsDir=`finddir B_USER_APPS_DIRECTORY`
 
@@ -16,22 +15,19 @@ if [ -d $sysAppsDir/Einsteinium ]
 then
 	appDir="$sysAppsDir/Einsteinium"
 	dataDir=`finddir B_SYSTEM_DATA_DIRECTORY`
-	binDir=`finddir B_SYSTEM_BIN_DIRECTORY`
 elif [ -d $userAppsDir/Einsteinium ]
 then
 	appDir="$userAppsDir/Einsteinium"
 	dataDir=`finddir B_USER_DATA_DIRECTORY`
-	binDir=`finddir B_USER_BIN_DIRECTORY`
 else
 	notify --group Einsteinium --type error --timeout 60 "There may have been a problem installing Einsteinium.  \
 The Launcher application could not be found."
 	exit 0
 fi
 
-# Create launch links
+# Copy bootscript to launch directory
 userBootDir=`finddir B_USER_BOOT_DIRECTORY`
-ln -s -f $binDir/einsteinium_daemon $userBootDir/launch
-ln -s -f $binDir/einsteinium_engine $userBootDir/launch
+cp -af $dataDir/EinsteiniumBootscript.sh $userBootDir/launch
 
 # Notification to user
 notify --group Einsteinium --onClickApp application/x-vnd.Einsteinium_Launcher --timeout 60 --icon "$dataDir/Einsteinium/notify_icon" \
