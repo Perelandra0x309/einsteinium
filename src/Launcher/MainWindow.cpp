21a1dfac5f0735845a7058c380571e2342b415fa
Brian Hill <supernova@warpmail.net>
2016-11-19 19:43:44 -0500

    Fix spacing issues in main window

--- src/Launcher/MainWindow.cpp
+++ src/Launcher/MainWindow.cpp
@@ -9,13 +9,12 @@ MainWindow::MainWindow(BRect size, window_look look)
 	BWindow(size, "Einsteinium Launcher", look, B_NORMAL_WINDOW_FEEL, B_NOT_ZOOMABLE, B_ALL_WORKSPACES)
 {
 	Lock();
-	const int inset = 0;
 	BRect frameRect(Bounds());
 //	frameRect.InsetBy(-1,0);
-	frameRect.bottom-=25;
+//	frameRect.bottom-=25;
 	fView = new MainView(frameRect);
 	fInfoView = new BStringView("InfoView","");
-//	fInfoView->SetExplicitMaxSize(BSize(100, 20));
+//	fInfoView->SetExplicitMaxSize(BSize(B_SIZE_UNSET, 8));
 /*	fAboutTextView = new BTextView(BRect(-10,0,0,1), "About text", BRect(0,0,0,0), B_FOLLOW_NONE);
 	fAboutTextView->SetText("");
 	fAboutTextView->MakeSelectable(false);
@@ -25,14 +24,14 @@ MainWindow::MainWindow(BRect size, window_look look)
 //	fAboutTextView->SetViewColor(fView->ViewColor());
 //	fAboutTextView->SetTextRect(BRect(0,0,999,40));*/
 
-
-	SetLayout(new BGroupLayout(B_VERTICAL));
-	AddChild(BGroupLayoutBuilder(B_VERTICAL)
+	BLayoutBuilder::Group<>(this, B_VERTICAL)
+		// Get rid of pixels at edges of list view
+		.SetInsets(-3, 0, -3, 3)
 		.Add(fView)
-		.Add(fInfoView)
-	//	.Add(fAboutTextView)
-		.SetInsets(inset, inset, inset, inset)
-	);
+		.AddGroup(B_VERTICAL, B_USE_DEFAULT_SPACING)
+			// align text vertically centered
+			.SetInsets(5, -8, 0, 0)
+			.Add(fInfoView);
 	/*
 	BRect frameRect(Bounds());
 	frameRect.InsetBy(-1,0);
@@ -45,7 +44,7 @@ MainWindow::MainWindow(BRect size, window_look look)
 	AddChild(fView);
 	AddChild(fInfoView);*/
 
-	SetSizeLimits(fView->TabFrame(fView->CountTabs()-1).right + 2*inset, 9999, 200, 9999);
+	SetSizeLimits(fView->TabFrame(fView->CountTabs()-1).right + 5, 9999, 200, 9999);
 	Unlock();
 }
 
